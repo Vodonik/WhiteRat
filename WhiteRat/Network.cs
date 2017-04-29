79361609811192564d8f58dc5856ff8621242dd1
Vodonik <george.b.power@gmail.com>
2017-04-29 17:19:47 +0200

    Start of Network class.

---
+++ WhiteRat/Network.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhiteRat
+{
+	class Network
+	{
+		private int[] configuration;
+		private int numberOfLayers;
+		public Layer[] layers;
+
+		public Network(int[] config)
+		{
+			configuration = config;
+			numberOfLayers = config.Length;
+
+			layers = new Layer[numberOfLayers];
+
+			for (int i = 0; i < numberOfLayers; i++)
+			{
+				int neuronsInLayer = configuration[i];
+				Layer layerToAdd = new Layer(neuronsInLayer);
+				for (int j = 0; j < neuronsInLayer; j++)
+				{
+					Neuron n;
+
+					if (i == 0)
+						n = new Neuron(0);
+					else
+						n = new Neuron(configuration[i - 1]);
+				}
+			}
+		}
+	}
+
+	struct Layer
+	{
+		public Neuron[] neurons;
+
+		public Layer(int neuronNumber)
+		{
+			neurons = new Neuron[neuronNumber];
+		}
+	}
+}
--- WhiteRat/Program.cs
+++ WhiteRat/Program.cs
@@ -8,8 +8,16 @@ namespace WhiteRat
 {
 	class Program
 	{
+		static Network net;
+
 		static void Main(string[] args)
 		{
+			int[] config = { 2, 1 };
+			net = new Network(config);
+
+			Console.WriteLine(net.layers);
+
+			Console.WriteLine("Got to here!");
 			Console.ReadLine();
 		}
 	}
